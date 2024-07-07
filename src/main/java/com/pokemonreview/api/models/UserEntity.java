054cfa342e082d3f41d14206b2ea8e06d2a31009
mohamed mahmoud <mohammed.mahmoud.elhawary@gmail.com>
2024-07-07 17:38:56 +0300

    updated security

--- src/main/java/com/pokemonreview/api/models/UserEntity.java
+++ src/main/java/com/pokemonreview/api/models/UserEntity.java
@@ -25,7 +25,6 @@ public class UserEntity {
 	private int id;
 	private String userName;
 	private String password;
-	
 	@ManyToMany
 	@JoinTable(name = "user_roles", joinColumns= @JoinColumn
 			  (name = "user_id",referencedColumnName = "id"),
